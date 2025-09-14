10d7a86d1e01d302f6c00aa5a678125ac97d838b
mrtanonur <onurtan@gmail.com>
2025-09-14 20:56:07 +0300

    feat(constants): add constants

---
+++ lib/utils/constants/asset_constants.dart
@@ -0,0 +1,7 @@
+class AssetConstants {
+  static const String appleLogo = "assets/images/apple_logo.png";
+  static const String bitespotTransparent =
+      "assets/images/bitespot_transparent.png";
+  static const String bitespot = "assets/images/bitespot.png";
+  static const String googleLogo = "assets/images/google_logo.png";
+}
